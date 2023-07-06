aa2c923261d6a8aab8688a8a5b0c435eb6e92505
Luisz576 <lrferlinalves@gmail.com>
2023-07-06 00:25:14 -0300

    comment update

--- Api/src/application/controllers/games/TheBridgeController.ts
+++ Api/src/application/controllers/games/TheBridgeController.ts
@@ -2,9 +2,6 @@ import IHttpContext from '../../../domain/interfaces/IHttpContext'
 import validator from '../../../services/validator'
 
 export default class TheBridgeController {
-    // TODO colocar redis nos controllers?
-    //      ou nos repositoies e ai quando chamar a funcao save no model salva no redis?
-    //      ou criar uma camada de cache?
     async store(httpContext: IHttpContext){
         return httpContext.getResponse().sendStatus(501)
     }
